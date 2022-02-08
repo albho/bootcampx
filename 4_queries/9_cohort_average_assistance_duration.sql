c2508f937bfe6dd73de4dbc290b217ce2ff8d537
Albert Ho <albertcyi.ho@gmail.com>
2022-02-08 10:29:04 -0800

    Select the average total duration of assistance requests for all cohorts

---
+++ 4_queries/9_cohort_average_assistance_duration.sql
@@ -0,0 +1,12 @@
+SELECT AVG(total_durations.total_duration) AS average_total_duration
+FROM (
+    SELECT cohorts.name AS cohort,
+      SUM(
+        assistance_requests.completed_at - assistance_requests.started_at
+      ) AS total_duration
+    FROM cohorts
+      INNER JOIN students ON cohorts.id = students.cohort_id
+      INNER JOIN assistance_requests ON students.id = assistance_requests.student_id
+    GROUP BY cohort
+    ORDER BY total_duration
+  ) AS total_durations;
