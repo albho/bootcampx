23713984655b5b0411e34353f8cd208932946675
Albert Ho <albertcyi.ho@gmail.com>
2022-02-09 14:37:59 -0800

    Add query for students and cohort info

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,3 +6,20 @@ const pool = new Pool({
   host: "localhost",
   database: "bootcampx",
 });
+
+pool
+  .query(
+    `
+SELECT students.id, students.name, cohorts.name AS cohort_name
+FROM students
+  INNER JOIN cohorts ON students.cohort_id = cohorts.id
+LIMIT 5;
+`
+  )
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(
+        `${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`
+      );
+    });
+  });
