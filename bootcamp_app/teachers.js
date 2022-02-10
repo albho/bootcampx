26c5d033b424c4e285dd80a9734831aeb445b9a4
Albert Ho <albertcyi.ho@gmail.com>
2022-02-09 17:23:38 -0800

    Sanitize inputs to prevent SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,22 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `
-    SELECT DISTINCT teachers.name AS teacher,
+const queryString = `
+  SELECT DISTINCT
+    teachers.name AS teacher,
     cohorts.name AS cohort
   FROM assistance_requests
     INNER JOIN teachers ON assistance_requests.teacher_id = teachers.id
     INNER JOIN students ON assistance_requests.student_id = students.id
     INNER JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
-  ORDER BY teacher;
-`
-  )
+  WHERE cohorts.name LIKE $1
+    ORDER BY teacher;
+  `;
+const cohortName = process.argv[2];
+const value = [`%${cohortName}%`];
+
+pool
+  .query(queryString, value)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
